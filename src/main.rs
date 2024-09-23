6b983e2b2fd24d934d1a84c332d4029c86a1c975
Trailblazer Isaiah <123408229+Trailblazer4@users.noreply.github.com>
2024-09-22 17:57:11 -0700

    replaced if-else with match

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use rand::Rng;
+use colored::*;
+use std::cmp::Ordering;
+
+fn user_guess(guess: &mut String) -> i32 {
+    println!("{}", "Guess a number: ".bright_cyan());
+
+    io::stdin()
+    .read_line(guess)
+    .expect("Failed to read line");
+
+    guess.trim()
+    .parse()
+    .expect("Failed to parse int from user guess")
+}
+
+fn main() {
+    // let randy: i32 = 1 + rand::random::<i32>().abs() % 10;
+    let randy: i32 = rand::thread_rng().gen_range(1, 100);
+
+    let mut guess: String = String::new();
+    
+    for _ in 0..7 {
+        let guess_i: i32 = user_guess(&mut guess);
+
+        println!("Your guess was {}.", &guess_i);
+
+        match guess_i.cmp(&randy) {
+            Ordering::Less => println!("{}\n{}", "D'oh! Try again!".red(), "Hint: go right".yellow()),
+            Ordering::Greater => println!("{}\n{}", "D'oh! Try again!".red(), "Hint: go left".yellow()),
+            Ordering::Equal => {
+                println!("{}", "Wow, you're really good at this!".green());
+                break;
+            },
+        }
+
+        guess.clear();
+    }
+    println!("The answer was {}.", randy);
+}
